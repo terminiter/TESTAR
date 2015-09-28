e5cef4ab8e73cee6197d96de1550056d80676f92
urueda <urueda@pros.upv.es>
2015-09-28 13:49:28 +0200

    java version feedback

--- testar/resources/testar.bat
+++ testar/resources/testar.bat
@@ -2,5 +2,7 @@
 where java> __javajdkbin.tmp
 set /p javajdkbin=<__javajdkbin.tmp
 del __javajdkbin.tmp
+echo TESTAR requires JDK 1.8, trying to run with:
+"%javajdkbin%" -version
 "%javajdkbin%" -ea -server -Xmx1g -jar testar.jar
 PAUSE
